69470974ca1cbea8ec44c42a5619f46c581bc68d
MarioGK <mariogk01@gmail.com>
2020-01-01 22:01:36 -0300

    Adding the csfml dlls, and copying them to the output

--- CrossUI.SFML/System/SharedLibName.cs
+++ CrossUI.SFML/System/SharedLibName.cs
@@ -4,13 +4,13 @@ namespace CrossUI.SFML.System
     public static class Csfml
     {
         /// <summary>CSFML Audio Lib Name</summary>
-        public const string Audio = "csfml-audio";
+        public const string Audio = "SFML\\csfml-audio";
         /// <summary>CSFML Graphics Lib Name</summary>
-        public const string Graphics = "csfml-graphics";
+        public const string Graphics = "SFML\\csfml-graphics";
         /// <summary>CSFML System Lib Name</summary>
-        public const string System = "csfml-system";
+        public const string System = "SFML\\csfml-system";
         /// <summary>CSFML Window Lib Name</summary>
-        public const string Window = "csfml-window";
+        public const string Window = "SFML\\csfml-window";
 
     }
 }
